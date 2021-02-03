d5b92065707168b4c3e715ecc28eae0fdcb66e93
spiderr <spiderr@bitweaver.org>
2021-02-03 16:19:17 -0500

    update path for kernel/includes/setup_inc.php

--- index.php
+++ index.php
@@ -16,7 +16,7 @@
 // | Authors: spider <spider@steelsun.com>
 // +----------------------------------------------------------------------+
 
-require_once( '../kernel/setup_inc.php' );
+require_once( '../kernel/includes/setup_inc.php' );
 require_once( SPHINX_PKG_INCLUDE_PATH.'sphinx_setup_inc.php' );
 global $gSphinxSystem;
 
